8db3f10b819fa7bd0e8c2cbc08b089417a09acc0
vwicky <viktir.zozulka@gmail.com>
2024-05-02 19:35:41 +0300

    Basic Fuctionallity added

---
+++ src/main/java/com/FameRivalsDemo/fameRivals/FameRivalsApplication.java
@@ -0,0 +1,13 @@
+package com.FameRivalsDemo.fameRivals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FameRivalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FameRivalsApplication.class, args);
+	}
+
+}
